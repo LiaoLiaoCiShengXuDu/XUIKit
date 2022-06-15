e9f769b2ecefc4a8d86373ca9f755c64796631d6
zyx <845709917@qq.com>
2022-06-15 16:15:15 +0800

    骨架

---
+++ Example/Pods/Target Support Files/XUIKit/XUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XUIKit : NSObject
+@end
+@implementation PodsDummy_XUIKit
+@end
---
+++ Example/XUIKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XUIKit
+//
+//  Created by LiaoLiaoCiShengXuDu on 06/15/2022.
+//  Copyright (c) 2022 LiaoLiaoCiShengXuDu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
